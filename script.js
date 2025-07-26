da34f4e6287017d81c2d553616bd26ee77984bd5
AKASH E <eakash0706@gmail.com>
2025-07-26 15:59:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function addTask() {
+  const input = document.getElementById('taskInput');
+  const task = input.value.trim();
+
+  if (task === '') return;
+
+  const li = document.createElement('li');
+  li.textContent = task;
+
+  const removeBtn = document.createElement('span');
+  removeBtn.textContent = '✖';
+  removeBtn.onclick = () => li.remove();
+
+  li.appendChild(removeBtn);
+  document.getElementById('taskList').appendChild(li);
+
+  input.value = '';
+}
